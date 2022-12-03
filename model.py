36c8e3a3de0b0970d21ec07746e7660688a32cfa
anonymous <anonymous@users.noreply.github.com>
2022-12-03 06:54:51 -0500

    model architecture

---
+++ model.py
@@ -0,0 +1,100 @@
+# Copyright 2016 Symantec Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+import abc
+import bisect
+import decimal
+import math
+import random
+
+import numpy as np
+
+
+class Model(metaclass=abc.ABCMeta):
+    """Abstract base class for models.
+
+    For each probability p, we handle its logprob -- i.e., with some
+    abuse of notation, the base-2 logarithm changed of sign:
+    -math.log2(p).
+    """
+
+    @abc.abstractmethod
+    def generate(self):
+        """Generate a random password according to the model.
+
+        Returns (logprob, passwd); passwd is the random password and
+        logprob is its probability.
+        """
+        pass
+
+    def sample(self, n):
+        """Generate a sample of n passwords."""
+        return (self.generate() for _ in range(n))
+
+    @abc.abstractmethod
+    def logprob(self, word):
+        """Return the logprob of word according to the model."""
+        pass
+
+
+
+class PosEstimator:
+
+    def __init__(self, sample, realsize=None, probabilistic_model = False, nn = True):
+        # realsize is a hack to make plot_restrictions work.
+        # don't use unless you know what you're doing!
+        if probabilistic_model == True and nn == True:
+            self.logprobs = logprobs = np.fromiter((lp for pw, lp in sample), float)
+        elif probabilistic_model == True and nn == False:
+            self.logprobs = logprobs = np.fromiter((lp for lp, _ in sample), float)
+        else:
+            self.logprobs = logprobs = sample
+        logprobs.sort()
+        if realsize is None:
+            realsize = len(logprobs)
+        logn = math.log2(realsize)
+        # unable to crack
+        self.positions = (2 ** (logprobs - logn)).cumsum()
+
+    def position(self, logprob, inclusive=True):
+        if inclusive:
+            idx = bisect.bisect_right(self.logprobs, logprob)
+        else:
+            idx = bisect.bisect_left(self.logprobs, logprob)
+        return self.positions[idx - 1] if idx > 0 else 0
+
+    def logpos(self, logprob):
+        return math.log2(self.position(logprob))
+
+    def logprob(self, pos):
+        return np.interp(math.log2(pos + 1), np.log2(self.positions + 1),
+                         self.logprobs)
+
+    def generate(self, model_generate, entropy):
+        lp_threshold = self.logprob(2 ** entropy)
+        for logprob, word in iter(model_generate, None):
+            if (logprob <= lp_threshold and
+                    lp_threshold < logprob - math.log2(random.random())):
+                return logprob, word
+
+    def sample(self, model_generate, entropy, n):
+        for _ in range(n):
+            yield self.generate(model_generate, entropy)
+
+
+class IPWEstimator:
+
+    def __init__(self, sample, store=lambda lp, word: (lp, word)):
+        sample = list(sample)
+        self.logn = logn = math.log2(len(sample))
+        self.ipw = [2 ** decimal.Decimal(lp - logn) for lp, _ in sample]
+        self.stored = [store(lp, word) for lp, word in sample]
+
+    def evaluate(self, fun):
+        return sum(w * fun(v)
+                   for w, v in zip(self.ipw, self.stored))
